8bfae20cde9dbc7ddfee2db1dcb344eedc84f8c7
Clervens Volcy <clervens.volcy@gmail.com>
2015-01-13 17:11:36 -0500

    MOD : enable emailing

--- server.js
+++ server.js
@@ -107,10 +107,38 @@ app.get('/', function(req, res) {
 		req: req
 	})
 });
-app.get('/contact', function(req, res){
+
+var email   = require("emailjs/email")
+	.server.connect({
+	   user:    process.env.SMTP_USERNAME, 
+	   password:process.env.SMTP_PASSWORD, 
+	   host:    "smtp.gmail.com", 
+	   ssl:     true
+	});
+
+app.route('/contact')
+.get(function(req, res){
 	res.render('contact', {
 		title: "Cvolcy GifMaker",
-		description: "Contact",		
+		description: "Contact",
+		submitted: false,
+		error: false,
+		req: req
+	});
+})
+.post(function(req, res){
+	var message = {
+	   text:    "i hope this works", 
+	   from:    "Clervens <clervens.volcy@gmail.com>", 
+	   to:      "clervens <clervens.volcy@gmail.com>",
+	   subject: "testing emailjs"
+	};
+	email.send(message, function(err, message) { console.log(err || message); });
+	res.render('contact', {
+		title: "Cvolcy GifMaker",
+		description: "Contact",
+		submitted: true,
+		error: false,
 		req: req
 	});
 });
