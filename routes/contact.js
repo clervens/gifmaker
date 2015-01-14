fa19fbf3d95e96c1ee7d6b245765e144621585f0
Clervens Volcy <clervens.volcy@gmail.com>
2015-01-13 19:01:54 -0500

    MOD : check for valid human check number.

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var crypto = require("../modules/crypto");
 var parseUrlEncoded = bodyParser.urlencoded({ extended: false });
 
 var email   = require("emailjs/email")
@@ -11,6 +12,14 @@ var email   = require("emailjs/email")
 	});
 
 var router = express.Router();
+router.use(parseUrlEncoded);
+
+function generateHumanCheck() {
+	var humanCheck = {nb1: Math.floor((Math.random() * 10) + 1),nb2: Math.floor((Math.random() * 10) + 1)};
+		humanCheck.check = crypto.encrypt(JSON.stringify(humanCheck));
+
+	return humanCheck;
+}
 
 router.route('/contact')
 	.get(function(req, res){
@@ -19,25 +28,32 @@ router.route('/contact')
 			description: "Contact",
 			submitted: false,
 			error: false,
+			humanCheck: generateHumanCheck(),
 			req: req
 		});
 	})
 	.post(parseUrlEncoded, function(req, res){
 		var message = {
-		   text:    req.params.message, 
-		   from:    req.params.name +" <"+req.params.email+">", 
-		   to:      "clervens <clervens.volcy@gmail.com>",
+		   text:    req.body.message, 
+		   from:    req.body.name +" <"+req.body.email+">", 
+		   to:      "<"+process.env.SMTP_USERNAME+">",
 		   subject: "GifMaker | Contact Request"
 		};
-		console.log(req.params);
+		var humanCheck = JSON.parse(crypto.decrypt(req.body.humancheck));
+		if (parseInt(req.body.real) != (humanCheck.nb1 + humanCheck.nb2)) {
+			message = {};
+		}
+		console.log(parseInt(req.body.real), JSON.parse(crypto.decrypt(req.body.humancheck)), message);
 		email.send(message, function(err, message) { 
 			res.render('contact', {
 				title: "Cvolcy GifMaker",
 				description: "Contact",
-				submitted: !err || true,
+				submitted: !err,
 				error: err,
+				humanCheck: generateHumanCheck(),	
 				req: req
 			});
+			console.log(err);
 		});
 	});
 
